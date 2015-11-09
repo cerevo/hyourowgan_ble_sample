6417dd3a4013a4aa6f0b1675c964f12920fa239c
Fuminori OKUHARA <fuminori@cerevo.com>
2015-11-09 19:23:56 +0900

    Android 6.0(API Level 23)対応:
    + 位置情報サービスへのアクセス許可を追加
      Android 6.0からLeScan実行に位置情報サービスへのアクセス権が必要になったため。
      6.0より古いバージョンはサポートしていない方法なのでMin SDKを23に変更

--- app/src/main/java/com/cerevo/blueninja/hyourowgan_ble_sample/MainActivity.java
+++ app/src/main/java/com/cerevo/blueninja/hyourowgan_ble_sample/MainActivity.java
@@ -1,12 +1,18 @@
 package com.cerevo.blueninja.hyourowgan_ble_sample;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //位置情報サービス許可
+    private static final int PERMISSION_REQUEST_LOCATION = 12345;
 
     Button buttonShowGPIO;
     Button buttonShowPWM;
@@ -14,9 +20,24 @@ public class MainActivity extends AppCompatActivity {
     Button buttonShowAirpressureSensor;
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            if ((grantResults.length > 0) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(getApplicationContext(), "Warning: Location service Disabled.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+        }
 
         buttonShowGPIO = (Button)findViewById(R.id.buttonShowGPIO);
         buttonShowGPIO.setOnClickListener(buttonClickListener);
